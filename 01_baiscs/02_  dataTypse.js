51bef2361a86801ff218ee9d9b5da63222cb1fca
CE REHMAN RRR <rehmanaslam858@gmail.com>
2025-03-11 07:22:38 +0000

    container.js

---
+++ 01_baiscs/02_  dataTypse.js
@@ -0,0 +1,19 @@
+1 //: treet all js code  as never virson
+
+alert (3+3) // we are using nodejs , not browser
+
+console.log (3+3) 
+
+1 ,Consol.LOG (rehman)
+
+
+
+
+
+let name = 
+let ,email =rehmanaslam858gmail.com
+let is =true/ false
+
+
+consol.log(typeof rehman)
+consol.log (rehman)
